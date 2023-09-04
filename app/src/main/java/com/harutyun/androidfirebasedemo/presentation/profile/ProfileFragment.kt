2c4784291e530d24e5d6a0f86b3ea9e24006d67d
Harutyun <git config --global user.name "FIRST_NAME LAST_NAME"harutyun@mishloha.co.il>
2023-09-04 19:02:27 +0400

    Added profile screen ui

---
+++ app/src/main/java/com/harutyun/androidfirebasedemo/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,29 @@
+package com.harutyun.androidfirebasedemo.presentation.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.harutyun.androidfirebasedemo.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private val profileViewModel: ProfileViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
